f8350ba485fa15237c67ef30705d4cfcba8301fc
Jehad <jehad.p.khattak@outlook.com>
2026-03-22 23:52:15 +0100

    Fix next.config.ts build error

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  // 1. Enable instrumentation hook to run polyfills on startup
-  experimental: {
-    instrumentationHook: true,
-  },
-  // 2. Turbopack is disabled in package.json dev, let's also disable in build just in case
-  // although it should be disabled by default in Next 15 production build
+  /* Standard konfigurasjon for Next.js 15 */
 };
 
 export default nextConfig;
